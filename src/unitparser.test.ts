b5784c1d1fa27bf755a44080f45024d46d6da495
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-18 21:18:55 +0100

    parseunit tests

---
+++ src/unitparser.test.ts
@@ -0,0 +1,81 @@
+type Prefix = { value: number; abbreviation: string; key: string }
+
+const SI_PREFIXES: Record<string, Prefix> = {
+    yotta: { value: 1e24, abbreviation: "Y", key: "yotta" },
+    zetta: { value: 1e21, abbreviation: "Z", key: "zetta" },
+    exa: { value: 1e18, abbreviation: "E", key: "exa" },
+    peta: { value: 1e15, abbreviation: "P", key: "peta" },
+    tera: { value: 1e12, abbreviation: "T", key: "tera" },
+    giga: { value: 1e9, abbreviation: "G", key: "giga" },
+    mega: { value: 1e6, abbreviation: "M", key: "mega" },
+    kilo: { value: 1e3, abbreviation: "k", key: "kilo" },
+    hecto: { value: 1e2, abbreviation: "h", key: "hecto" },
+    deca: { value: 1e1, abbreviation: "da", key: "deca" },
+    deci: { value: 1e-1, abbreviation: "d", key: "deci" },
+    centi: { value: 1e-2, abbreviation: "c", key: "centi" },
+    milli: { value: 1e-3, abbreviation: "m", key: "milli" },
+    micro: { value: 1e-6, abbreviation: "µ", key: "micro" },
+    nano: { value: 1e-9, abbreviation: "n", key: "nano" },
+    pico: { value: 1e-12, abbreviation: "p", key: "pico" },
+    femto: { value: 1e-15, abbreviation: "f", key: "femto" },
+    atto: { value: 1e-18, abbreviation: "a", key: "atto" },
+    zepto: { value: 1e-21, abbreviation: "z", key: "zepto" },
+    yocto: { value: 1e-24, abbreviation: "y", key: "yocto" },
+} as const
+
+type Unit = { value: number; abbreviation: string; si?: true }
+
+const LENGTH_UNITS: Record<string, Unit> = {
+    meter: { value: 1, abbreviation: "m", si: true },
+    inch: { value: 0.0254, abbreviation: "in" },
+    foot: { value: 0.3048, abbreviation: "ft" },
+    yard: { value: 0.9144, abbreviation: "yd" },
+    mile: { value: 1609.344, abbreviation: "mi" },
+} as const
+
+type PrefixedUnit = { prefix?: Prefix; unit: Unit }
+
+function findPrefix(input: string): Prefix | undefined {
+    const prefix_entry = Object.entries(SI_PREFIXES).find(([prefixName]) =>
+        input.startsWith(prefixName)
+    )
+    if (prefix_entry) {
+        return prefix_entry[1]
+    }
+}
+
+function parseUnit(input: string): PrefixedUnit {
+    const found_prefix = findPrefix(input)
+    if (LENGTH_UNITS[input]) {
+        return { unit: LENGTH_UNITS[input] }
+    } else if (found_prefix) {
+        const unit_string = input.slice(found_prefix.key.length)
+        const unit = LENGTH_UNITS[unit_string]
+        if (!unit) {
+            throw new Error(`Unknown unit: ${unit_string}`)
+        }
+        return { prefix: found_prefix, unit: LENGTH_UNITS.meter }
+    } else {
+        throw new Error(`Unknown unit: ${input}`)
+    }
+}
+
+test("parse meter", () => {
+    expect(parseUnit("meter").unit).toBe(LENGTH_UNITS.meter)
+})
+
+test("parse inch", () => {
+    expect(parseUnit("inch").unit).toBe(LENGTH_UNITS.inch)
+})
+
+test("parse kilometer", () => {
+    const { unit, prefix } = parseUnit("kilometer")
+    expect(unit).toBe(LENGTH_UNITS.meter)
+    expect(prefix).toBe(SI_PREFIXES.kilo)
+})
+
+test("parse millimeter", () => {
+    const { unit, prefix } = parseUnit("millimeter")
+    expect(unit).toBe(LENGTH_UNITS.meter)
+    expect(prefix).toBe(SI_PREFIXES.milli)
+})
