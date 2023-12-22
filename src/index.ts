818d0f550dcb856ea031d517777082a7a9ac516e
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-22 17:49:58 +0100

    write stdout

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/bin/env node
 
+import { exit, stdout } from "process"
 import { convertUnit } from "./converter"
 
 const quantities = [
@@ -17,7 +18,10 @@ function main(args: string[]) {
 
     const value_in = parseFloat(value)
 
-    console.log(convertUnit(value_in, unit_in, unit_out))
+    const result = convertUnit(value_in, unit_in, unit_out)
+
+    stdout.write(result.toString())
+    exit()
 }
 
 main(process.argv.slice(2))
