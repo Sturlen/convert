32377e4cd8c99b358fb0c8e413a8d23336cbe0cb
sturlen <43607012+Sturlen@users.noreply.github.com>
2023-12-22 17:47:25 +0100

    make case sensitive

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,8 @@ function main(args: string[]) {
     const [value, unit_in, unit_out] = args
 
     const value_in = parseFloat(value)
-    const unit_in_normalized = unit_in.toLowerCase()
-    const unit_out_normalized = unit_out.toLowerCase()
 
-    console.log(convertUnit(value_in, unit_in_normalized, unit_out_normalized))
+    console.log(convertUnit(value_in, unit_in, unit_out))
 }
 
 main(process.argv.slice(2))
